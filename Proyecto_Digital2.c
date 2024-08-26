bdead85869a15ca0813bb06596a6a6704b9332d6
Grimaceee <166880871+Grimaceee@users.noreply.github.com>
2024-08-26 11:22:57 -0600

    Commit #2

--- Proyecto_Digital2.c
+++ Proyecto_Digital2.c
@@ -25,3 +25,19 @@ const int dp = 5;
 const int digito1 = 14;
 const int digito2 = 12;
 const int digito3 = 13;
+
+Servo mi_servo;  // Objeto para controlar el servo
+
+// segmentos para dígitos del 0-9
+byte segmentos[] = {
+    0b00111111, // 0
+    0b00000110, // 1
+    0b01011011, // 2
+    0b01001111, // 3
+    0b01100110, // 4
+    0b01101101, // 5
+    0b01111101, // 6
+    0b00000111, // 7
+    0b01111111, // 8
+    0b01101111  // 9
+};
